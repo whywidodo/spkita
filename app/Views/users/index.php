a8df39ae6f01677fa8a9daaa0d68599a994bdc7e
farahardelia <farahardelia13@gmail.com>
2022-12-13 10:07:01 +0700

    menambahkan form pada profil users

--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -31,6 +31,34 @@
                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed natus eos, molestias error asperiores doloribus, totam ex libero soluta neque nulla tempora quasi. Consectetur, saepe culpa. Aliquam ducimus veritatis reiciendis.
                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo unde quasi rerum tempora incidunt tempore amet aliquam atque optio necessitatibus eum, quas error accusantium pariatur illo distinctio modi quam quos.
                   </span>
+                  <h1>Pengaturan Akun</h1>
+                  <form action="post">
+                    <div class="form-group row">
+                        <label for="inputNama" class="col-sm-2 col-form-label">Nama Lengkap</label>
+                           <div class="col-sm-10">
+                              <input type="text" class="form-control" id="inputnama" placeholder="Nama Lengkap">
+                           </div>
+                     </div>
+                     <div class="form-group row">
+                        <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
+                           <div class="col-sm-10">
+                              <input type="text" class="form-control" id="inputUsername" placeholder="Username">
+                           </div>
+                     </div>
+                     <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                           <div class="col-sm-10">
+                              <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                           </div>
+                     </div>
+                     <div class="form-group row">
+                        <label for="inputUlangiPassword" class="col-sm-2 col-form-label">Ulangi Password</label>
+                           <div class="col-sm-10">
+                              <input type="password" class="form-control" id="inputUlangiPassword" placeholder="Ulangi Password">
+                           </div>
+                     </div>
+                     <button type="update" class="btn btn-primary">Update</button>
+                  </form>
                </div>
             </div>
          </div>
