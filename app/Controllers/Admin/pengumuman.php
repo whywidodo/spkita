6f43b88d74e228d6e0e1c18df1d056ba1de70c96
whywidodo <mr.wahyuwidodo@gmail.com>
2023-01-05 11:15:49 +0700

    update administrator feature

--- app/Controllers/Admin/pengumuman.php
+++ app/Controllers/Admin/pengumuman.php
@@ -3,11 +3,30 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\PengumumanModel;
 
 class Pengumuman extends BaseController
 {
+   protected $pengumumanModel;
+   public function __construct()
+   {
+      $this->pengumumanModel = new PengumumanModel();
+   }
+
    public function index()
    {
-      return view('admin/pengumuman');
+      $data = [
+         'bobotNilai' => 3,
+         'bobotPenghasilan' => 5,
+         'bobotTanggungan' => 4,
+         'dataPengumuman' => $this->pengumumanModel->getPengumuman(),
+         'dataNilai' => $this->pengumumanModel->getNilaiMax(),
+         'dataPenghasilan' => $this->pengumumanModel->getPenghasilanMin(),
+         'dataTanggungan' => $this->pengumumanModel->getTanggunganMax(),
+      ];
+      // dd($data['dataPengumuman']['0']['nisn']);
+      // dd($data);
+
+      return view('admin/pengumuman', $data);
    }
 }
